28ef6adac98193de64ea1514e9609c339f1f3083
shunta.kokubu <shunta.kokubu@metateam.co.jp>
2026-05-13 14:27:07 +0900

    修正箇所の変更

--- src/main/java/jp/co/metateam/library/service/BookMstService.java
+++ src/main/java/jp/co/metateam/library/service/BookMstService.java
@@ -1,34 +1,33 @@
 package jp.co.metateam.library.service;
- 
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
- 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
- 
+
 import io.micrometer.common.util.StringUtils;
 import jp.co.metateam.library.model.BookMst;
 import jp.co.metateam.library.model.BookMstDto;
 import jp.co.metateam.library.repository.BookMstRepository;
- 
+
 @Service
 public class BookMstService {
- 
+
     private final BookMstRepository bookMstRepository;
-   
+
     @Autowired
-    public BookMstService(BookMstRepository bookMstRepository){
+    public BookMstService(BookMstRepository bookMstRepository) {
         this.bookMstRepository = bookMstRepository;
     }
-   
+
     public List<BookMstDto> findAvailableWithStockCount() {
         List<BookMst> books = this.bookMstRepository.findLimitedBook();
         List<BookMstDto> bookMstDtoList = new ArrayList<BookMstDto>();
- 
         // 書籍の在庫数を取得
         // FIXME: 現状は書籍ID毎にDBに問い合わせている。一度のSQLで完了させたい。
         for (int i = 0; i < books.size(); i++) {
@@ -39,20 +38,15 @@ public class BookMstService {
             bookMstDto.setTitle(book.getTitle());
             bookMstDtoList.add(bookMstDto);
         }
- 
         return bookMstDtoList;
     }
+
     @Transactional
     public void insert(BookMstDto dto) {
- 
-    BookMst book = new BookMst();
-            book.setIsbn(dto.getIsbn());
-            book.setTitle(dto.getTitle());
-       
-    bookMstRepository.save(book);//データベースに保存
+        BookMst book = new BookMst();
+        book.setIsbn(dto.getIsbn());
+        book.setTitle(dto.getTitle());
 
-}
+        bookMstRepository.save(book);//データベースに保存
     }
-
-
-
+}
